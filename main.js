f46a2d09a44ed3ea7c941f777fabfbd83f6d37e8
KArpi <karpad2@gmail.com>
2019-06-23 07:59:56 +0200

    Mobile version

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
-var config = require('./config');
-var server= require('./server')
-
+//var app = require("app"),
+config = require('./config');
+var server=require("./www/server");
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
